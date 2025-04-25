f2b8f5048a3913d8bcc3ff6239aa39b68ab11fdb
I_m_pramay <pramay2822n@gmail.com>
2025-04-25 17:41:47 +0100

    halfDone

---
+++ src/api.js
@@ -0,0 +1,11 @@
+//products 
+export const productList = "https://fakestoreapi.com/products"
+
+//images
+export const manModel = "https://imgofluxora.blob.core.windows.net/images/manModel.jpg?sp=r&st=2025-04-10T22:22:25Z&se=2025-09-30T06:22:25Z&sv=2024-11-04&sr=b&sig=tMJN1JC%2B%2BWXZtTtRtjMRidZbOwtfoMieAbCza8WkaZI%3D"
+export const womenModel ="https://imgofluxora.blob.core.windows.net/images/womanModel.jpg?sp=r&st=2025-04-10T22:23:43Z&se=2025-09-30T06:23:43Z&sv=2024-11-04&sr=b&sig=pHCIz74AwCePsW40Ba06slVnMlJg1EgJONZv5J%2F2cJo%3D"
+export const childModel = "https://imgofluxora.blob.core.windows.net/images/ChildModel.jpg?sp=r&st=2025-04-10T22:11:14Z&se=2025-09-30T06:11:14Z&sv=2024-11-04&sr=b&sig=9SC0fzpmwUZuas3GMhdXXxvR3W7SeCRBQUA5NIx1%2FfA%3D"
+export const femaleModel ="https://imgofluxora.blob.core.windows.net/images/WomanModel2.jpg?sp=r&st=2025-04-10T22:19:09Z&se=2025-10-28T07:19:09Z&sv=2024-11-04&sr=b&sig=1hR5nhNFvZ3mZldN3KGU1lolOY%2BeoDSrxlkMuQf10So%3D"
+export const female = "https://imgofluxora.blob.core.windows.net/images/feamale1.png?sp=r&st=2025-04-25T14:37:11Z&se=2026-05-26T22:37:11Z&spr=https&sv=2024-11-04&sr=b&sig=4mhp0MDTZS27JYOaivVpTuKyq7AasZx4Vk3hG%2Fb4XcA%3D"
+export const manAndWoman =  "https://imgofluxora.blob.core.windows.net/images/manAndWoman.png?sp=r&st=2025-04-25T14:19:59Z&se=2026-03-31T22:19:59Z&spr=https&sv=2024-11-04&sr=b&sig=DJZzcz8r6L1VOY7OzuVp6ICBuTfht%2BhOQju%2B7Mzoshc%3D"
+export const threeModels = "https://imgofluxora.blob.core.windows.net/images/3models.PNG?sp=r&st=2025-04-25T15:38:25Z&se=2026-04-28T23:38:25Z&spr=https&sv=2024-11-04&sr=b&sig=wee4lmy95C6WDPEGH0x2qoezmrCYYr%2BjhxJp7OOALOc%3D"
